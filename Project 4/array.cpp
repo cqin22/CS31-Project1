19fa00ca4b7e3496cc9284bb51776a0ba4d45cb4
cqin22 <103715748+cqin22@users.noreply.github.com>
2022-11-02 17:34:01 -0700

    Update array.cpp

--- Project 4/array.cpp
+++ Project 4/array.cpp
@@ -215,5 +215,130 @@ int split(string a[], int n, string splitter)
 int main()
 {
 
+    string people[5] = {"boris", "gordon", "rishi", "liz", "john"};
+    cerr << "appendToAll(people, -4, \"!!!\") = " << appendToAll(people, -4, "!!!") << endl; // test negative
+    cerr << appendToAll(people, 3, "  ");
+    for (int i = 0; i < sizeof(people) / sizeof(string); i++)
+    {
+        cerr << people[i] << " ";
+    }
+    cerr << "\n\n";
+
+    string people1[6] = {"boris", "gordon", "rishi", "liz", "john", "gordon"};
+    cerr << "lookup(people1, 6, \"gordon\") = " << lookup(people1, 6, "gordon") << endl; // find earliest gordon
+    cerr << "lookup(people1, 6, \"arthur\") = " << lookup(people1, 6, "arthur") << endl; // what if target string doesn't exist
+    cerr << "\n";
+
+    string pm[7] = {"david", "liz", "margaret", "tony", "gordon", "boris", "tony"};
+    cerr << "positionOfMax(pm, 7) = " << positionOfMax(pm, 7) << endl; // finds first instance of alphabetically-last name
+    string pm1[0] = {};
+    cerr << "positionOfMax(pm1, 0) = " << positionOfMax(pm1, 0) << endl; // if no interesting elements
+    cerr << "\n";
+
+    string mp[5] = {"john", "david", "liz", "theresa", "margaret"};
+    string mp2[12] = {"john", "david", "liz", "theresa", "margaret", "sarah", "is", "goofy", "that", "was", "a", "joke"};
+
+    cerr << "rotateLeft(mp, 5, 1) = " << rotateLeft(mp, 5, 1) << endl; // tests a normal scenario
+    for (int i = 0; i < sizeof(mp) / sizeof(string); i++)
+    { // look at new array
+        cerr << mp[i] << " ";
+    }
+    cerr << endl;
+
+    cerr << "rotateLeft(mp2, 6, 3) = " << rotateLeft(mp2, 6, 3) << endl; // tests a normal scenario
+    for (int i = 0; i < sizeof(mp2) / sizeof(string); i++)
+    { // look at new array
+        cerr << mp2[i] << " ";
+    }
+
+    cerr << endl;
+    string mp1[5] = {"john", "david", "liz", "theresa", "margaret"};
+    cerr << "rotateLeft(mp1, 5, -4) = " << rotateLeft(mp1, 5, -4) << endl; // check out of bounds negative pos
+    cerr << "rotateLeft(mp1, 5, 10) = " << rotateLeft(mp1, 5, 10) << endl; // check out of bounds out of bounds pos
+    cerr << "rotateLeft(mp1, -5, 3) = " << rotateLeft(mp1, -5, 3) << endl; // negative n
+    cerr << "\n";
+
+    string d[9] = {"tony", "boris", "rishi", "rishi", "gordon", "gordon", "gordon", "rishi", "rishi"};
+    cerr << "countRuns(d, 9) = " << countRuns(d, 9) << endl;   // counts 5 sequences
+    cerr << "countRuns(d, -9) = " << countRuns(d, -9) << endl; // tests negative
+    cerr << "countRuns(d, 9) = " << countRuns(d, 9) << endl;
+    cerr << "countRuns(d, 9) = " << countRuns(d, 6) << endl; // counts 6 sequences
+                                                             // counts 5 sequences
+    cerr << "countRuns(d, 0) = " << countRuns(d, 0) << endl; // counts 6 sequences
+
+    cerr << "\n";
+
+    string leader[6] = {"boris", "rishi", "ab", "tony", "theresa", "david"};
+    cerr << "flip(leader, 4) = " << flip(leader, 4) << endl; // tests first 4 swap
+    for (int i = 0; i < sizeof(leader) / sizeof(string); i++)
+    { // look at new array
+        cerr << leader[i] << " ";
+    }
+    cerr << endl;
+    string leader1[7] = {"boris", "rishi", "ab", "tony", "theresa", "david", "arthur"};
+    cerr << "flip(leader1, 7) = " << flip(leader1, 7) << endl; // tests an odd-numbered array flip
+    for (int i = 0; i < sizeof(leader1) / sizeof(string); i++)
+    { // look at new array
+        cerr << leader1[i] << " ";
+    }
+    cerr << "\n\n";
+
+    string leader2[6] = {"boris", "rishi", "", "tony", "theresa", "david"};
+    string politician[5] = {"boris", "rishi", "david", "", "tony"};
+    cerr << "differ(leader2, 6, politician, 5) = " << differ(leader2, 6, politician, 5) << endl;   // normal test, should return index 2
+    cerr << "differ(leader2, 2, politician, 1) = " << differ(leader2, 2, politician, 1) << endl;   // when boths n's run out
+    cerr << "differ(leader2, -3, politician, 1) = " << differ(leader2, -3, politician, 1) << endl; // check negative n
+    cerr << "\n";
+
+    string names[10] = {"john", "margaret", "theresa", "rishi", "boris", "liz"};
+    string names1[10] = {"margaret", "theresa", "rishi"};
+    string names2[10] = {"john", "rishi"};
+    string names3[0] = {};
+    string names4[0] = {};
+    string names7[2] = {"e", "g"};
+    string names6[7] = {"a", "b", "c", "d", "e", "f", "g"};
+
+    cerr << "subsequence(names, 6, names1, 3) = " << subsequence(names, 6, names1, 3) << endl;   // normal test, found a subsequence at index 1
+    cerr << "subsequence(names, 5, names2, 2) = " << subsequence(names, 5, names2, 2) << endl;   // test w/ no subsequence in names
+    cerr << "subsequence(names, 6, names3, 0) = " << subsequence(names, 6, names3, 0) << endl;   // test with names3 as an empty array
+    cerr << "subsequence(names3, 0, names4, 0) = " << subsequence(names3, 0, names4, 0) << endl; // 2 empty subsequences, should still return 0
+    cerr << "subsequence(names3, 0, names2, 6) = " << subsequence(names3, 0, names2, 6) << endl; // test an empty array as man sequence and an actual array as subsequence, should return -1
+    cerr << "subsequence(names6, 0, names7, 6) = " << subsequence(names6, 7, names7, 2) << endl; // test an empty array as man sequence and an actual array as subsequence, should return -1
+    cerr << "\n";
+
+    string names5[10] = {"john", "margaret", "theresa", "rishi", "boris", "liz"};
+    string set1[10] = {"david", "boris", "rishi", "margaret"};
+    string set2[10] = {"tony", "gordon"};
+    string set3[0] = {};
+    cerr << "lookupAny(names5, 6, set1, 4) = " << lookupAny(names5, 6, set1, 4) << endl; // normal test, should return 1
+    cerr << "lookupAny(names5, 6, set2, 2) = " << lookupAny(names5, 6, set2, 2) << endl; // what if nothing from set2 exists in names5, returns -1
+    cerr << "lookupAny(names5, 6, set3, 0) = " << lookupAny(names5, 6, set3, 0) << endl; // empty set, should return -1
+    cerr << "lookupAny(names5, 0, set2, 2) = " << lookupAny(names5, 0, set2, 2) << endl; // empty set, should return -1
+
+    cerr << "\n";
+
+    string pm2[6] = {"david", "liz", "margaret", "tony", "gordon", "boris"};
+    string pm3[4] = {"margaret", "theresa", "liz", "rishi"};
+    cerr << "split(pm2, 6, \"john\") = " << split(pm2, 6, "john") << endl; // normal test
+    for (int i = 0; i < sizeof(pm2) / sizeof(string); i++)
+    {
+        cerr << pm2[i] << " ";
+    }
+    cerr << endl;
+    cerr << "split(pm3, 4, \"rishi\") = " << split(pm3, 4, "rishi") << endl; // includes the splitter itself in the string array
+    for (int i = 0; i < sizeof(pm3) / sizeof(string); i++)
+    {
+        cerr << pm3[i] << " ";
+    }
+    cerr << "\n\n";
+
+    string l[6] = {"boris", "rishi", "", "tony", "theresa", "david"};
+    int q = flip(l, 4); // returns 4
+                        // leader now contains:  "tony"  ""  "rishi"  "boris"  "theresa"  "david"
+    cerr << q << endl;
+    for (int i = 0; i < 6; i++)
+    {
+        cerr << l[i] << " ";
+    }
     return 0;
 }
